462802c79ed5905a56533efc36d3f2f74d15c108
Pablwo Araújo <pablwo.araujo@ccc.ufcg.edu.br>
2021-05-18 14:52:34 -0300

    Add tooltips

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import Image from 'ol/layer/Image';
 import ImageWMS from 'ol/source/ImageWMS';
+import Overlay from 'ol/Overlay';
+
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+var overlay = new Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250,
+  },
+});
+
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
 
 const selected = {
   municipios: false,
@@ -14,74 +33,81 @@ const selected = {
   etanol: false
 }
 
+var municipios = new Image({
+  title: 'municipios',
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/wms',
+    params: { 'LAYERS': 'cite:municipios_pr_pol_p31982_e50_a2014' },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+})
+
+var rodovias = new Image({
+  title: 'Rodovias Unificadas (2013) [SEIL/DER]',
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/wms',
+    params: { 'LAYERS': 'cite:rodovias_unificadas_lin_p29192_a2013' },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+})
+
+var hidrografia = new Image({
+  title: 'Hidrografia Generalizada [SEMA_AGUASPARANA]',
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/wms',
+    params: { 'LAYERS': 'cite:hidrografia_generalizada_lin_p31982_e50_a2011_v002' },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+})
+    
+var policia_1 = new Image({
+  title: 'Polícia Rodoviária Estadual [SESP]',
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/wms',
+    params: { 'LAYERS': 'cite:policia_rodoviaria_estadual_pto_p4674' },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+})
+
+var universidades = new Image({
+  title: 'Dados Universidades IEES [SETI]',
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/wms',
+    params: { 'LAYERS': 'cite:dado_universidade_iees_pto_p29192' },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+})
+
+var etanol = new Image({
+  title: 'Usinas de Etanol',
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/wms',
+    params: { 'LAYERS': 'cite:usinas_etanol_pto_p4674' },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+})
+
 const layers = {
-  municipios: {
-    image: new Image({
-      title: 'municipios',
-      source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/wms',
-        params: { 'LAYERS': 'cite:municipios_pr_pol_p31982_e50_a2014' },
-        ratio: 1,
-        serverType: 'geoserver'
-      })
-    })
-  },
-  rodovias: {
-    image: new Image({
-      title: 'Rodovias Unificadas (2013) [SEIL/DER]',
-      source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/wms',
-        params: { 'LAYERS': 'cite:rodovias_unificadas_lin_p29192_a2013' },
-        ratio: 1,
-        serverType: 'geoserver'
-      })
-    })
-  },
-  hidrografia: {
-    image: new Image({
-      title: 'Hidrografia Generalizada [SEMA_AGUASPARANA]',
-      source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/wms',
-        params: { 'LAYERS': 'cite:hidrografia_generalizada_lin_p31982_e50_a2011_v002' },
-        ratio: 1,
-        serverType: 'geoserver'
-      })
-    })
-  },
-  policia_1: {
-    image: new Image({
-      title: 'Polícia Rodoviária Estadual [SESP]',
-      source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/wms',
-        params: { 'LAYERS': 'cite:policia_rodoviaria_estadual_pto_p4674' },
-        ratio: 1,
-        serverType: 'geoserver'
-      })
-    })
-  },
-  universidades: {
-    image: new Image({
-      title: 'Dados Universidades IEES [SETI]',
-      source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/wms',
-        params: { 'LAYERS': 'cite:dado_universidade_iees_pto_p29192' },
-        ratio: 1,
-        serverType: 'geoserver'
-      })
-    })
-  },
-  etanol: {
-    image: new Image({
-      title: 'Usinas de Etanol',
-      source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/wms',
-        params: { 'LAYERS': 'cite:usinas_etanol_pto_p4674' },
-        ratio: 1,
-        serverType: 'geoserver'
-      })
-    })
-  }
+  municipios: municipios,
+  rodovias: rodovias,
+  hidrografia: hidrografia,
+  policia_1: policia_1,
+  universidades: universidades,
+  etanol: etanol
 }
+
+var view = new View({
+  center: [0, 0],
+  zoom: 0
+});
+
+var viewProjection = view.getProjection();
 
 const map = new Map({
   target: 'map',
@@ -90,10 +116,8 @@ const map = new Map({
       source: new OSM()
     }),
   ],
-  view: new View({
-    center: [0, 0],
-    zoom: 0
-  })
+  overlays: [overlay],
+  view: view
 });
 
 document.getElementById('municipios').addEventListener('click', setLayer, false)
@@ -109,12 +133,80 @@ function setLayer(value) {
   let is_selected = selected[layer];
   if (!is_selected) {
     selected[layer] = true;
-    map.addLayer(layers[layer].image);
+    map.addLayer(layers[layer]);
   } else {
     selected[layer] = false;
-    map.removeLayer(layers[layer].image);
+    map.removeLayer(layers[layer]);
   }
   if (checkbox) {
     checkbox.checked = !is_selected;
+  }
+}
+
+map.on('singleclick', function(evt){
+  var coordinate = evt.coordinate;
+  var viewResolution = /** @type {number} */ (view.getResolution());
+
+  var urlMunicipios = municipios.getSource().getFeatureInfoUrl(
+    coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'}
+  )
+  var urlRodovias = rodovias.getSource().getFeatureInfoUrl(
+    coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'}
+  )
+  var urlHidrografia = hidrografia.getSource().getFeatureInfoUrl(
+    coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'}
+  )
+  var urlPolicia = policia_1.getSource().getFeatureInfoUrl(
+    coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'}
+  )
+  var urlUniversidades = universidades.getSource().getFeatureInfoUrl(
+    coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'}
+  )
+  var urlEtanol = etanol.getSource().getFeatureInfoUrl(
+    coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'}
+  )
+
+  var urls = new Map()
+  urls.set('municipios', urlMunicipios)
+  urls.set('rodovias', urlRodovias)
+  urls.set('hidrografia', urlHidrografia)
+  urls.set('policia_1', urlPolicia)
+  urls.set('universidades', urlUniversidades)
+  urls.set('etanol', urlEtanol)
+  
+  for (const [key, value] of Object.entries(selected)) {
+    if(value){
+      getAjaxResult(urls.get(key), coordinate)
+    }
+  }
+})
+
+function getAjaxResult(urlParam, coordinate) {
+  if(urlParam){
+    $.ajax({
+      url: urlParam,
+      crossDomain: true,
+      success: function(result){
+        handleResult(result, coordinate)
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+        console.log('erro: '+ errorThrown)
+      }
+    })
+  }
+}
+
+function handleResult(result, coordinate){
+  var html = subStringBody(result);
+  content.innerHTML = result;
+  overlay.setPosition(coordinate);
+}
+
+function subStringBody(html){
+  var i = html.indexOf("<body>");
+  var f = html.indexOf("</body>");
+  if(i>=0 && f>=0){
+    i+=6.
+    return html.substring(i, f);
   }
 }
