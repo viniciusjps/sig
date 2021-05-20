7f6d0c9d38d601fcbba4477bfd3a95f3afa0f32b
EduardoMCF <eduardo.freitas@ccc.ufcg.edu.br>
2021-05-19 23:33:40 -0300

    Added the last 2 queries

--- src/index.js
+++ src/index.js
@@ -114,7 +114,7 @@ var etanol = new Image({
 
 const joinParams = function (params, params_names) {
   let result = ''
-  for (let i = 0; i < params.length; index++) {
+  for (let i = 0; i < params.length; i++) {
     result += `${params_names[i]}:${params[i]};`
   }
   result = result.slice(0,-1);
@@ -161,10 +161,44 @@ const queryPoliciaRodovia = function (params) {
   const view_params = joinParams(params, params_names);
 
   return new Image({
-              title: 'Munícipios que estão a ... metros da rodovia ...',
+              title: 'Sedes de polícia estadual que estão a ... metros da rodovia ...',
               source: new ImageWMS({
                       url: baseUrl,
                       params: { 'LAYERS': 'cite:policia_rodovia',
+                                'viewparams': view_params
+                              },
+                      ratio: 1,
+                      serverType: 'geoserver'
+              })
+  })
+}
+
+const queryComprimentoRodovia = function (params) {
+  const params_names = ['rod'];
+  const view_params = joinParams(params, params_names);  
+
+  return new Image({
+              title: 'Comprimento de da rodovia ...',
+              source: new ImageWMS({
+                      url: baseUrl,
+                      params: { 'LAYERS': 'cite:com_rod',
+                                'viewparams': view_params
+                              },
+                      ratio: 1,
+                      serverType: 'geoserver'
+              })
+  })
+}
+
+const queryEtanolMesorregiao = function (params) {
+  const params_names = ['meso'];
+  const view_params = joinParams(params, params_names);  
+
+  return new Image({
+              title: 'Usinas de etanol localizadas na mesoregião ...',
+              source: new ImageWMS({
+                      url: baseUrl,
+                      params: { 'LAYERS': 'cite:etanol_meso',
                                 'viewparams': view_params
                               },
                       ratio: 1,
@@ -250,6 +284,20 @@ map.on('singleclick', function (evt) {
   var urlEtanol = etanol.getSource().getFeatureInfoUrl(
     coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'}
   )
+  var urlQueryMunicipios = (saved_queries.queryMunicipios) ? 
+    (saved_queries.queryMunicipios.getSource().getFeatureInfoUrl(coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'})) : undefined
+
+  var urlQueryRios = (saved_queries.queryRios) ? 
+    (saved_queries.queryRios.getSource().getFeatureInfoUrl(coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'})) : undefined
+  
+  var urlQueryPoliciaRodovia = (saved_queries.queryPoliciaRodovia) ? 
+    (saved_queries.queryPoliciaRodovia.getSource().getFeatureInfoUrl(coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'})) : undefined
+  
+  var urlQueryComprimentoRodovia = (saved_queries.queryComprimentoRodovia) ? 
+  (saved_queries.queryComprimentoRodovia.getSource().getFeatureInfoUrl(coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'})) : undefined
+
+  var urlQueryEtanolMesorregiao = (saved_queries.queryEtanolMesorregiao) ? 
+  (saved_queries.queryEtanolMesorregiao.getSource().getFeatureInfoUrl(coordinate, viewResolution, viewProjection, {'INFO_FORMAT':'text/html'})) : undefined
 
   var urls = new Map()
   urls.set('municipios', urlMunicipios)
@@ -259,8 +307,19 @@ map.on('singleclick', function (evt) {
   urls.set('policia_1', urlPolicia)
   urls.set('universidades', urlUniversidades)
   urls.set('etanol', urlEtanol)
+  urls.set('queryMunicipios', urlQueryMunicipios)
+  urls.set('queryRios', urlQueryRios)
+  urls.set('queryPoliciaRodovia', urlQueryPoliciaRodovia)
+  urls.set('queryComprimentoRodovia', urlQueryComprimentoRodovia)
+  urls.set('queryEtanolMesorregiao', urlQueryEtanolMesorregiao)
 
   for (const [key, value] of Object.entries(selected)) {
+    if (value) {
+      getAjaxResult(urls.get(key), coordinate)
+    }
+  }
+
+  for (const [key, value] of Object.entries(selected_queries)) {
     if (value) {
       getAjaxResult(urls.get(key), coordinate)
     }
@@ -284,7 +343,7 @@ function getAjaxResult(urlParam, coordinate) {
 
 function handleResult(result, coordinate) {
   var html = subStringBody(result);
-  content.innerHTML = result;
+  content.innerHTML = html;
   overlay.setPosition(coordinate);
 }
 
@@ -300,29 +359,36 @@ function subStringBody(html) {
 document.getElementById('btn_queryMunicipios').addEventListener('click', addQuery, false)
 document.getElementById('btn_queryRios').addEventListener('click', addQuery, false)
 document.getElementById('btn_queryPoliciaRodovia').addEventListener('click', addQuery, false)
-
+document.getElementById('btn_queryComprimentoRodovia').addEventListener('click', addQuery, false)
+document.getElementById('btn_queryEtanolMesorregiao').addEventListener('click', addQuery, false)
 
 const selected_queries = {
   queryMunicipios: false,
   queryRios: false,
-  queryPoliciaRodovia: false
+  queryPoliciaRodovia: false,
+  queryComprimentoRodovia: false,
+  queryEtanolMesorregiao: false
 }
 
 const queries = {
   queryMunicipios: queryMunicipios,
   queryRios: queryRios,
-  queryPoliciaRodovia: queryPoliciaRodovia
+  queryPoliciaRodovia: queryPoliciaRodovia,
+  queryComprimentoRodovia: queryComprimentoRodovia,
+  queryEtanolMesorregiao: queryEtanolMesorregiao
 }
 
 const saved_queries = {
   queryMunicipios: undefined,
   queryRios: undefined,
-  queryPoliciaRodovia: undefined
+  queryPoliciaRodovia: undefined,
+  queryComprimentoRodovia: undefined,
+  queryEtanolMesorregiao: undefined
 }
 
 function addQuery(value) {
   let id = value.target.id.split('btn_')[1];
-  let params = document.getElementsByClassName(`param_${id}`).map(elem => elem.value);
+  let params = Array.from(document.getElementsByClassName(`param_${id}`)).map(elem => elem.value);
   console.log(params);
 
   let is_selected = selected_queries[id];
